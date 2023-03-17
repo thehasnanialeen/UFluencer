eef5231ca5a1e795398035ecc21f51e1d913cdd9
amaanjiwani11 <97082643+amaanjiwani11@users.noreply.github.com>
2023-03-17 10:07:12 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -79,18 +79,18 @@ function App() {
               <Carousel variant='dark' indicators={false}>
                 <Carousel.Item interval={3000}>
                   <img src='./logo192.png'></img>
-                  <span className="card-title ms-1 fs-4 fw-bold">ShoppersPartner</span>
-                  <p className="card-text text-center my-2">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam convallis elit in velit fringilla, sit amet dictum sapien hendrerit. Phasellus non libero ac odio pulvinar vestibulum sit amet in massa. Integer aliquam velit vel massa dictum, et euismod quam dignissim. Donec a diam vel dolor venenatis blandit. Curabitur tincidunt ante at nulla posuere vestibulum. Donec lacinia justo eget magna bibendum, a hendrerit tortor fringilla. Duis commodo est vitae libero varius efficitur. Vestibulum non massa commodo, ultricies risus id, commodo lorem. Vestibulum semper lectus tellus, a lacinia massa commodo vel. Donec egestas molestie ex, vel consequat risus malesuada nec. Nulla ullamcorper blandit malesuada. Sed fermentum mi et dui molestie ultricies."</p>
+                  <span className="card-title ms-1 fs-4 fw-bold">BrainStorm</span>
+                  <p className="card-text text-center my-2">"UFluence has been a game changer for our influencer marketing campaigns. The platform is easy to use and has helped us connect with the right influencers for our brand. The analytics and insights have also been extremely helpful in measuring the success of our campaigns.We've tried several influencer marketing platforms, but UFluence stands out in terms of their customer service and support. The team is always responsive and goes above and beyond to help us achieve our goals."</p>
                 </Carousel.Item>
                 <Carousel.Item interval={3000}>
                   <img src='./logo192.png'></img>
-                  <span className="card-title ms-1 fs-4 fw-bold">ShoppersPartner</span>
-                  <p className="card-text text-center my-2">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam convallis elit in velit fringilla, sit amet dictum sapien hendrerit. Phasellus non libero ac odio pulvinar vestibulum sit amet in massa. Integer aliquam velit vel massa dictum, et euismod quam dignissim. Donec a diam vel dolor venenatis blandit. Curabitur tincidunt ante at nulla posuere vestibulum. Donec lacinia justo eget magna bibendum, a hendrerit tortor fringilla. Duis commodo est vitae libero varius efficitur. Vestibulum non massa commodo, ultricies risus id, commodo lorem. Vestibulum semper lectus tellus, a lacinia massa commodo vel. Donec egestas molestie ex, vel consequat risus malesuada nec. Nulla ullamcorper blandit malesuada. Sed fermentum mi et dui molestie ultricies."</p>
+                  <span className="card-title ms-1 fs-4 fw-bold">Superrr</span>
+                  <p className="card-text text-center my-2">"UFluence has exceeded our expectations in terms of the quality of influencers we've been able to connect with. The platform is intuitive and user-friendly, and we've seen a significant increase in engagement and conversions since incorporating influencer marketing into our strategy"</p>
                 </Carousel.Item>
                 <Carousel.Item interval={3000}>
                   <img src='./logo192.png'></img>
-                  <span className="card-title ms-1 fs-4 fw-bold">ShoppersPartner</span>
-                  <p className="card-text text-center my-2">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam convallis elit in velit fringilla, sit amet dictum sapien hendrerit. Phasellus non libero ac odio pulvinar vestibulum sit amet in massa. Integer aliquam velit vel massa dictum, et euismod quam dignissim. Donec a diam vel dolor venenatis blandit. Curabitur tincidunt ante at nulla posuere vestibulum. Donec lacinia justo eget magna bibendum, a hendrerit tortor fringilla. Duis commodo est vitae libero varius efficitur. Vestibulum non massa commodo, ultricies risus id, commodo lorem. Vestibulum semper lectus tellus, a lacinia massa commodo vel. Donec egestas molestie ex, vel consequat risus malesuada nec. Nulla ullamcorper blandit malesuada. Sed fermentum mi et dui molestie ultricies."</p>
+                  <span className="card-title ms-1 fs-4 fw-bold">Kingfisher Enterprises</span>
+                  <p className="card-text text-center my-2">"As a small business, we were hesitant to invest in influencer marketing, but UFluence made it accessible and affordable. We've been able to connect with influencers who align with our values and mission, and the platform has helped us grow our brand in a meaningful way. UFluence has been instrumental in helping us expand our reach and connect with our target audience. The platform's data-driven approach has allowed us to make informed decisions about our influencer campaigns, and we've seen a significant return on investment as a result."</p>
                 </Carousel.Item>
             </Carousel>
           </div>
@@ -105,9 +105,10 @@ function App() {
             <h2>About us</h2>
           </div>
           <div className='card-body'>
-            <p className="card-text text-center">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam convallis elit in velit fringilla, sit amet dictum sapien hendrerit. Phasellus non libero ac odio pulvinar vestibulum sit amet in massa. Integer aliquam velit vel massa dictum, et euismod quam dignissim. Donec a diam vel dolor venenatis blandit. Curabitur tincidunt ante at nulla posuere vestibulum. Donec lacinia justo eget magna bibendum, a hendrerit tortor fringilla. Duis commodo est vitae libero varius efficitur. Vestibulum non massa commodo, ultricies risus id, commodo lorem. Vestibulum semper lectus tellus, a lacinia massa commodo vel. Donec egestas molestie ex, vel consequat risus malesuada nec. Nulla ullamcorper blandit malesuada. Sed fermentum mi et dui molestie ultricies."</p>
-            <p className="card-text text-center">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam convallis elit in velit fringilla, sit amet dictum sapien hendrerit. Phasellus non libero ac odio pulvinar vestibulum sit amet in massa. Integer aliquam velit vel massa dictum, et euismod quam dignissim. Donec a diam vel dolor venenatis blandit. Curabitur tincidunt ante at nulla posuere vestibulum. Donec lacinia justo eget magna bibendum, a hendrerit tortor fringilla. Duis commodo est vitae libero varius efficitur. Vestibulum non massa commodo, ultricies risus id, commodo lorem. Vestibulum semper lectus tellus, a lacinia massa commodo vel. Donec egestas molestie ex, vel consequat risus malesuada nec. Nulla ullamcorper blandit malesuada. Sed fermentum mi et dui molestie ultricies."</p>
-            {/* <a href="#services" className="hero-cta">Learn more</a> */}
+            <p className="card-text text-center">"At UFluence, we believe in the power of influencer marketing to help businesses reach their target audience and achieve their goals, both locally and globally. Our platform was born out of a passion for connecting brands with the right influencers, in a way that is efficient, transparent, and data-driven. Our team is made up of experts in influencer marketing, technology, and customer service. We are dedicated to providing our users with the best possible experience, whether they are based in their local community or expanding to new markets around the world. Our approach is glocal, meaning that we combine global expertise with local knowledge to create tailored solutions for our users."</p>
+            <p className="card-text text-center">"We understand that influencer marketing can be intimidating, especially for small businesses or those new to the industry. That's why we've designed UFluence to be user-friendly, intuitive, and accessible to all, regardless of their location. Whether you're a startup looking to boost your social media presence in your local market, or an established brand seeking new ways to connect with audiences on a global scale, UFluence has the tools and resources to help you succeed."</p>
+            <p className="card-text text-center">"Above all, we are committed to transparency, integrity, and delivering measurable results for our users. We believe in building long-term partnerships with our customers, and we are always here to provide guidance, support, and expertise every step of the way. Thank you for considering UFluence for your influencer marketing needs. We look forward to helping you achieve your goals and grow your business, both locally and globally, with our glocal approach."</p>
+          {/* <a href="#services" className="hero-cta">Learn more</a> */}
           </div>
         </div>
 
@@ -158,23 +159,23 @@ function App() {
             <ul className="pricing-list">
               <li className="pricing-item me-3">
                 <div className="pricing-header">
-                  <h3 className="pricing-title">Starter</h3>
-                  <p className="pricing-price">$100/month</p>
+                  <h3 className="pricing-title">Basic</h3>
+                  <p className="pricing-price">$449.99/month</p>
                 </div>
                 <ul className="pricing-features">
-                  <li>1 campaign per month</li>
-                  <li>Up to 5 influencers</li>
+                  <li>Access to 20 influencers shortlisted for the needs</li>
+                  <li>Can choose to interact with 10 influencers</li>
                   <li>Basic reporting</li>
                 </ul>
               </li>
               <li className="pricing-item">
                 <div className="pricing-header">
-                  <h3 className="pricing-title">Business</h3>
-                  <p className="pricing-price">$500/month</p>
+                  <h3 className="pricing-title">Premium</h3>
+                  <p className="pricing-price">$679.99/month</p>
                 </div>
                 <ul className="pricing-features">
-                  <li>3 campaigns per month</li>
-                  <li>Up to 20 influencers</li>
+                  <li>Access to 50 influencers shortlisted for the needs</li>
+                  <li>Can choose to interact with 30 influencers</li>
                   <li>Advanced reporting</li>
                 </ul>
               </li>
